e5962b0cdacfdfd280e06bcfbefe502225b226ee
fady nagy <88649929+fady-nk@users.noreply.github.com>
2022-01-06 18:58:55 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,52 +1,75 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
+#include <cmath>
 using namespace std;
+
 int main()
 {
-long long  A,B,C;
-cin>>A>>B>>C;
-if(A<B && A<C ){
-   if(B<C){
-    cout<<A<<endl;
-    cout<<B<<endl;
-    cout<<C<<endl;
-    }
-    else {
-      cout<<A<<endl;
-      cout<<C<<endl;
-      cout<<B<<endl;
-    }
-}
- else if (B<A && B<C )  {
-        if(A<C){
-    cout<<B<<endl;
-    cout<<A<<endl;
-    cout<<C<<endl;
-    }
-    else{
-        cout<<B<<endl;
-        cout<<C<<endl;
-        cout<<A<<endl;
-    }
- }
-if(C<A && C<B){
-    if(A<B){
-        cout<<C<<endl;
-        cout<<A<<endl;
-        cout<<B<<endl;
-    }
-    else{
-        cout<<C<<endl;
-        cout<<B<<endl;
-        cout<<A<<endl;
-    }
-}
+    //declare variables
+   float a=0.0,b=0.0,c=0.0,sum=0,a2,v=0;
 
-cout<<endl;
-cout<<A<<endl;
-cout<<B<<endl;
-cout<<C<<endl;
+   //get input from user
+   cin>>a>>b>>c;
 
-return 0;
+   //operations to know type of triangle
+
+   //arrange sides
+   if(a>b &&a>c)
+    a=a;
+
+   else if(b>a && b>c){
+    v=a;
+    a=b;
+    b=v;
+    }
+
+    else if(c>a && c>b){
+       v=a;
+       a=c;
+       c=v;}
+
+
+sum= pow(b,2)+pow(c,2);
+    a2=pow(a,2);
+
+
+ if(a>=(b+c)){
+
+    cout<<"NAO FORMA TRIANGULO\n";}
+
+
+ else if(a2==sum || a2>sum){
+    if(a2==sum){
+    cout<<"TRIANGULO RETANGULO\n";
+      if(a==b && a==c)
+      cout<<"TRIANGULO EQUILATERO\n";
+
+      else if(a==b || b==c || a==c)
+      cout<<"TRIANGULO ISOSCELES\n";}
+
+   else if(a2>sum){
+    cout<<"TRIANGULO OBTUSANGULO\n";
+
+      if(a==b && a==c)
+      cout<<"TRIANGULO EQUILATERO\n";
+
+      else if(a==b || b==c || a==c)
+      cout<<"TRIANGULO ISOSCELES\n";}
+
+                    }
+
+
+
+
+
+else if(a2<sum){
+
+    cout<<"TRIANGULO ACUTANGULO\n";
+      if(a==b && a==c)
+           cout<<"TRIANGULO EQUILATERO\n";
+
+      else if(a==b || b==c || a==c)
+          cout<<"TRIANGULO ISOSCELES\n";
+
+   }
+    return 0;
 }
